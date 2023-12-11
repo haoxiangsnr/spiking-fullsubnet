8f2e9fc5909e405540ea523c4bdb7715f4857728
haoxiangsnr@gmail.com <haoxiangsnr@gmail.com>
2023-12-11 02:31:57 +0000

    chore: remove docker configs

--- launch/backup/start.sh
+++
@@ -1,6 +0,0 @@
-cd /mnt/private_xianghao/proj/audiozen/
-pip install -e . -i https://mirrors.tencent.com/pypi/simple/
-
-cd /mnt/private_xianghao/proj/audiozen/recipes/dns_interspeech_2020
-
-torchrun --nnodes=1 --nproc_per_node=8 run.py -C
